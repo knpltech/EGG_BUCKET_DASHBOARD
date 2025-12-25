c06dfeb9c5c1c8fbada6bd7723a2dd85dad97078
Ameena.G <ameenag7781@gmail.com>
2025-12-25 12:09:07 +0530

    Update SignIn.jsx

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -18,18 +18,15 @@ export default function SignIn() {
 
     setLoading(true);
 
-    try {
-      fetch("https://egg-bucket-dashboard-1.onrender.com/api/auth/signin", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    username,
-    password,
-    role,
-  }),
-});
+    const res = await fetch(
+  "https://egg-bucket-dashboard-1.onrender.com/api/auth/signin",
+  {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password, role }),
+  }
+);
+
 
       const data = await res.json();
       setLoading(false);
